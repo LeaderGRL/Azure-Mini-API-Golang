4a8c9c411892da472463f39467217b033a580538
Jojo <jordangrilly@gmail.com>
2023-05-23 15:59:32 +0200

    Update : Prepare for Azure + serve all files

--- main/main.go
+++ main/main.go
@@ -13,11 +13,27 @@ import (
 )
 
 func main() {
+	dir, _ := os.Getwd()
+	fmt.Println(dir)
 	fs := http.FileServer(http.Dir("../View"))
 	http.Handle("/", fs)
 	http.HandleFunc("/users", getUsers)
+	http.HandleFunc("/createuser/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../View/CreateUser.html")
+	})
 	http.HandleFunc("/users/create", CreateUser)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Azure App Service sets the port as an Environment Variable
+	// This can be random, so needs to be loaded at startup
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+
+	// default back to 8080 for local dev
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Listening on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
@@ -53,6 +69,7 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "../DB/test.db")
 	if err != nil {
+		fmt.Println("Error SQL !")
 		log.Fatal(err)
 	}
 	defer db.Close()
@@ -62,11 +79,18 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	var user API.Users
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Printf("error decoding response: %v", err)
+		if e, ok := err.(*json.SyntaxError); ok {
+			log.Printf("syntax error at byte offset %d", e.Offset)
+		}
+		log.Printf("sakura response: %q", r.Body)
+		fmt.Println("Error Decode !")
 		log.Fatal(err)
 	}
 
 	result, err := db.Exec("INSERT INTO users (username, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Username, user.Password, user.Email, user.Created_At, user.Updated_At)
 	if err != nil {
+		fmt.Println("Error Insert !")
 		log.Fatal(err)
 	}
 
