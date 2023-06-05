b935816ed589ec44cbccb7120aa75b3d84ae4a55
Jojo <jordangrilly@gmail.com>
2023-06-05 09:17:33 +0200

    Update : Deploy config

--- main/main.go
+++ main/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"API"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -13,18 +17,18 @@ func main() {
 		fmt.Fprintf(w, "Hello World !")
 	})
 
-	// 	dir, _ := os.Getwd()
-	// 	fmt.Println(dir)
-	// 	fs := http.FileServer(http.Dir("../View"))
-	// 	http.Handle("/", fs)
-	// 	http.HandleFunc("/users", getUsers)
-	// 	http.HandleFunc("/createuser/", func(w http.ResponseWriter, r *http.Request) {
-	// 		http.ServeFile(w, r, "../View/CreateUser.html")
-	// 	})
-	// 	http.HandleFunc("/users/create", CreateUser)
+	// dir, _ := os.Getwd()
+	// fmt.Println(dir)
+	fs := http.FileServer(http.Dir("./View"))
+	http.Handle("/", fs)
+	http.HandleFunc("/users", getUsers)
+	http.HandleFunc("/createuser/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./View/CreateUser.html")
+	})
+	http.HandleFunc("/users/create", CreateUser)
 
-	// 	// Azure App Service sets the port as an Environment Variable
-	// 	// This can be random, so needs to be loaded at startup
+	// Azure App Service sets the port as an Environment Variable
+	// This can be random, so needs to be loaded at startup
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 
 	// default back to 8080 for local dev
@@ -34,75 +38,81 @@ func main() {
 
 	fmt.Println("Listening on port " + port)
 	http.ListenAndServe(":"+port, nil)
-	//log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-// func getUsers(w http.ResponseWriter, r *http.Request) {
-// 	dir, _ := os.Getwd()
-// 	fmt.Println(dir)
-// 	db, err := sql.Open("sqlite3", "../DB/test.db")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer db.Close()
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Get Users !")
+	// dir, _ := os.Getwd()
+	// fmt.Println(dir)
+	db, err := sql.Open("sqlite3", "./DB/test.db")
+	if err != nil {
+		fmt.Println("Error SQL !")
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-// 	rows, err := db.Query("SELECT * FROM users")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer rows.Close()
+	fmt.Fprintf(w, "db open !")
 
-// 	var users []API.Users
-// 	for rows.Next() {
-// 		var user API.Users
-// 		err := rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Created_At, &user.Updated_At)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		users = append(users, user)
-// 	}
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println("Error Query !")
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(users)
-// }
+	fmt.Fprintf(w, "row !")
 
-// func CreateUser(w http.ResponseWriter, r *http.Request) {
-// 	db, err := sql.Open("sqlite3", "../DB/test.db")
-// 	if err != nil {
-// 		fmt.Println("Error SQL !")
-// 		log.Fatal(err)
-// 	}
-// 	defer db.Close()
+	// 	var users []API.Users
+	// 	for rows.Next() {
+	// 		var user API.Users
+	// 		err := rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Created_At, &user.Updated_At)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 		users = append(users, user)
+	// 	}
 
-// 	// body, _ := ioutil.ReadAll(r.Body)
-// 	// log.Println(string(body))
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	w.WriteHeader(http.StatusOK)
+	// 	json.NewEncoder(w).Encode(users)
+}
 
-// 	var user API.Users
-// 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-// 		log.Printf("error decoding response: %v", err)
-// 		if e, ok := err.(*json.SyntaxError); ok {
-// 			log.Printf("syntax error at byte offset %d", e.Offset)
-// 		}
-// 		log.Printf("sakura response: %q", r.Body)
-// 		fmt.Println("Error Decode !")
-// 		log.Fatal(err)
-// 	}
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "../DB/test.db")
+	if err != nil {
+		fmt.Println("Error SQL !")
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-// 	result, err := db.Exec("INSERT INTO users (username, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Username, user.Password, user.Email, user.Created_At, user.Updated_At)
-// 	if err != nil {
-// 		fmt.Println("Error Insert !")
-// 		log.Fatal(err)
-// 	}
+	// body, _ := ioutil.ReadAll(r.Body)
+	// log.Println(string(body))
 
-// 	id, err := result.LastInsertId()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	var user API.Users
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Printf("error decoding response: %v", err)
+		if e, ok := err.(*json.SyntaxError); ok {
+			log.Printf("syntax error at byte offset %d", e.Offset)
+		}
+		log.Printf("sakura response: %q", r.Body)
+		fmt.Println("Error Decode !")
+		log.Fatal(err)
+	}
 
-// 	user.Id = int(id)
+	result, err := db.Exec("INSERT INTO users (username, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Username, user.Password, user.Email, user.Created_At, user.Updated_At)
+	if err != nil {
+		fmt.Println("Error Insert !")
+		log.Fatal(err)
+	}
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusCreated)
-// 	json.NewEncoder(w).Encode(user)
-// }
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user.Id = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
